a7f3ad9821d6bdc870c30d9fe050a8a56c2f8186
Andres Felipe Herrera Moreno <aherrera@truora.com>
2019-07-26 19:55:05 -0500

    Second version of minimax

--- modelHex/HexAgent.js
+++ modelHex/HexAgent.js
@@ -1,10 +1,10 @@
 const Agent = require('ai-agents').Agent;
-const AlphaBetaConstructor = require('alphabeta')
+var AlphaBetaConstructor = require('alphabeta')
 const Graph = require('node-dijkstra');
 const getEmptyHex = require('./getEmptyHex');
 
 var config = {
-    scoreFunction: scoreFunction,
+    scoreFunction: boardPath,
     generateMoves: posibleMovements,
     checkWinConditions: goalTest,
     depth: 3,
@@ -27,12 +27,16 @@ class HexAgent extends Agent {
         let available = getEmptyHex(board);
         let nTurn = size * size - available.length;
         config.state = board;
-        let alphabeta = AlphaBetaConstructor( config )
+        let alphabeta = AlphaBetaConstructor(config)
+        alphabeta.allSteps(function (beststate) {
+            console.log(beststate)
+            return
+        })
 
-        if (nTurn%0 == 0) { // First move
+        if (nTurn % 0 == 0) { // First move
             return [Math.floor(size / 2), Math.floor(size / 2) - 1];
-        } 
-            return [Math.floor(size / 2), Math.floor(size / 2)];
+        }
+        return [Math.floor(size / 2), Math.floor(size / 2)];
     }
 
 }
@@ -78,15 +82,15 @@ function posibleMovements(board) {
     let nextPosibleMovements = []
     let size = board.length;
     let nTurn = size * size - movements.length;
-    let player = 1
-    if (nTurn%2 !== 0) {
-        player = 2
+    let player = "1"
+    if (nTurn % 2 !== 0) {
+        player = "2"
     }
 
-    for(i=0; i < movements.length; i++) {
+    for (let i = 0; i < movements.length; i++) {
         let newBoard = _copyBoard(board)
         let movement = movements[i]
-        newBoard[movement/size + movement%size] = player
+        newBoard[Math.floor(movement / size)][Math.floor(movement % size)] = player
         nextPosibleMovements.push(newBoard)
     }
 
@@ -96,6 +100,12 @@ function posibleMovements(board) {
 function boardPath(board, callbackPuntuation) {
     let player = '1';
     let size = board.length;
+    let movements = getEmptyHex(board)
+    let nTurn = size * size - movements.length;
+    let boardC = _copyBoard(board)
+    if (nTurn % 0 !== 0) {
+        boardC = transpose(boardC)
+    }
 
     const route = new Graph();
 
@@ -103,7 +113,7 @@ function boardPath(board, callbackPuntuation) {
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             let key = i * size + j;
-            let list = getNeighborhood(key, player, board);
+            let list = getNeighborhood(key, player, boardC);
             let neighbors = {};
             list.forEach(x => {
                 neighbors[x + ''] = 1;
@@ -122,10 +132,10 @@ function boardPath(board, callbackPuntuation) {
     let neighborsX = {};
 
     for (let i = 0; i < size; i++) {
-        if (board[i][0] === 0) {
+        if (boardC[i][0] === 0) {
             neighborsT[(i * size) + ''] = 1;
         }
-        if (board[i][size - 1] === 0) {
+        if (boardC[i][size - 1] === 0) {
             neighborsX[(i * size + size - 1) + ''] = 1;
         }
     }
@@ -157,6 +167,89 @@ function transpose(board) {
     return boardT;
 }
 
-function _copyBoard( board ) {
+function _copyBoard(board) {
     return JSON.parse(JSON.stringify(board))
+}
+
+/**
+ * Return an array of the neighbors of the currentHex that belongs to the same player. The 
+ * array contains the id of the hex. id = row * size + col
+ * @param {Number} currentHex 
+ * @param {Number} player 
+ * @param {Matrix} board 
+ */
+function getNeighborhood(currentHex, player, board) {
+    let size = board.length;
+    let row = Math.floor(currentHex / size);
+    let col = currentHex % size;
+    let result = [];
+    let currentValue = board[row][col];
+    board[row][col] = 'x';
+    // Check the six neighbours of the current hex
+    pushIfAny(result, board, player, row - 1, col);
+    pushIfAny(result, board, player, row - 1, col + 1);
+    pushIfAny(result, board, player, row, col + 1);
+    pushIfAny(result, board, player, row, col - 1);
+    pushIfAny(result, board, player, row + 1, col);
+    pushIfAny(result, board, player, row + 1, col - 1);
+
+    board[row][col] = currentValue;
+
+    return result;
+}
+
+function pushIfAny(result, board, player, row, col) {
+    let size = board.length;
+    if (row >= 0 && row < size && col >= 0 && col < size) {
+        if (board[row][col] === player) {
+            result.push(...getNeighborhood(col + row * size, player, board));
+        } else if (board[row][col] === 0) {
+            result.push(col + row * size);
+        }
+    }
+}
+
+/**
+ * Chech if there exist a path from the currentHex to the target side of the board
+ * @param {Number} currentHex 
+ * @param {Number} player 
+ * @param {Matrix} board 
+ */
+function check(currentHex, player, board) {
+    if (isEndHex(currentHex, player, board.length)) {
+        return true;
+    }
+    let neighbors = getNeighborhood(currentHex, player, board);
+    for (let neighbor of neighbors) {
+        let size = board.length;
+        let row = Math.floor(neighbor / size);
+        let col = neighbor % size;
+        // setVisited(neighbor, player, board);
+        board[row][col] = -1;
+        let res = check(neighbor, player, board);
+        // resetVisited(neighbor, player, board);
+        board[row][col] = player;
+        if (res == true) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Chech if the current hex is a the opposite border of the board
+ * @param {Number} currentHex 
+ * @param {Number} player 
+ * @param {Number} size 
+ */
+function isEndHex(currentHex, player, size) {
+    if (player === "1") {
+        if ((currentHex + 1) % size === 0) {
+            return true;
+        }
+    } else if (player === "2") {
+        if (Math.floor(currentHex / size) === size - 1) {
+            return true;
+        }
+    }
 }
